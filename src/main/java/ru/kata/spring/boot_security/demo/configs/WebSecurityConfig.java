0abb788773e62c52495f79786c6b007c8a2299a2
dem0N4k <demON4k1993@gmail.com>
2024-01-03 19:18:46 +0300

    пробное (в процессе)

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -22,57 +22,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/", "/index").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin().successHandler(successUserHandler)
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .permitAll();
-//    }
-
-        http.formLogin()
-                .loginPage("/")
-                .successHandler(successUserHandler)
-                // указываем action с формы логина
-                .loginProcessingUrl("/login")
-                // Указываем параметры логина и пароля с формы логина
-                .usernameParameter("j_username")
-                .passwordParameter("j_password")
-                // даем доступ к форме логина всем
-                .permitAll();
-
-        http.logout()
-                // разрешаем делать логаут всем
-                .permitAll()
-                // указываем URL логаута
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                // указываем URL при удачном логауте
-                .logoutSuccessUrl("/login?logout")
-                //выключаем кросс-доменную секьюрность (на этапе обучения неважна)
-                .and().csrf().disable(); //- попробуйте выяснить сами, что это даёт
-
         http
-                // делаем страницу регистрации недоступной для авторизированных пользователей
                 .authorizeRequests()
-
-                .antMatchers("/").permitAll() // доступность всем
-
-                //страница аутентификации доступна всем
-                .antMatchers("/login").anonymous()
-
-                // защищенные URL
-
-                //.antMatchers("/user/**").access("hasAnyRole('USER')") // разрешаем входить на /user пользователям с ролью User
-                //.antMatchers("/admin/**").access("hasRole('ADMIN')")
-                //.antMatchers("/vip/**").access("hasRole('VIP')")
-                .antMatchers("/user/**").hasRole("USER")
+                .antMatchers("/", "/login").permitAll()
+                .antMatchers("/user/**").hasAnyRole("USER","ADMIN")
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/vip/**").hasRole("VIP")
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and()
+//
+                .formLogin()
+//                .loginPage("/login")
+                .successHandler(successUserHandler)
+//                .loginProcessingUrl("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .permitAll()
+                .and()
+
+                .logout().permitAll()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .and()
+                .csrf().disable();
+//
+////                //выключаем кросс-доменную секьюрность (на этапе обучения неважна)
+////                .and().csrf().disable(); //- попробуйте выяснить сами, что это даёт
     }
     // аутентификация inMemory
     @Bean
